b58d5a07dac3c9b274469c9513cf08387c92b9d3
djwoun <65102751+djwoun@users.noreply.github.com>
2021-06-23 00:54:22 +0900

    Create gol.cpp

---
+++ gol.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <conio.h>
+#include <cstdlib>
+#include <time.h>
+#include <Windows.h>
+using namespace std;
+
+
+void gotoxy(int x, int y)
+{
+    COORD coordinates;
+    coordinates.X = x;
+    coordinates.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
+}
+
+int main() {
+    int i, j, data[83][60], next[83][60];
+    int around;
+    srand(time(NULL));
+
+
+    for (i = 0; i <= 79; i++)
+        for (j = 0; j <= 56; j++)
+        {
+
+            data[i][j] = 0;
+
+            if (i > 1 && i < 78 && j>1 && j < 55)
+
+                if (rand() % 10 == 1) {
+                    //gotoxy(i, j); cout << "\2";
+                    data[i][j] = 1;
+
+                }
+
+        }
+
+    while (1) {
+
+        for (i = 2; i <= 77; i++)
+            for (j = 2; j <= 55; j++)
+            {
+                gotoxy(i, j);
+                if (data[i][j])cout << "\2"; else cout << " ";
+            }
+
+
+
+
+        for (i = 2; i <= 77; i++)
+            for (j = 2; j <= 55; j++)
+            {
+
+
+                next[i][j] = data[i][j];
+
+                around = data[i - 1][j - 1] + data[i][j - 1] + data[i + 1][j - 1] + data[i - 1][j] + data[i + 1][j] + data[i - 1][j + 1] + data[i][j + 1] + data[i + 1][j + 1];
+
+                if (data[i][j]==1) {
+                    if (around < 2 || around >3) {
+                        next[i][j] = 0;
+                    }
+                    else {
+                        next[i][j] = 1;
+                    }
+
+                }
+                else
+                {
+                    if (around == 3) next[i][j] = 1;
+
+                }
+
+            }
+                for (i = 2; i <= 77; i++)
+                    for (j = 2; j <= 55; j++)
+                    
+                        data[i][j] = next[i][j];
+                        
+                    
+
+
+                Sleep(5000);
+            
+
+
+                
+        
+    }
+    return 0;
+}
